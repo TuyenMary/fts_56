ea2a2d691ad1dda2b986a8748945460ddb324f33
tuyenmary <tuyenmary@gmail.com>
2016-06-23 13:30:28 +0700

    signup-signin-signout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
+
   has_many :activities, dependent: :destroy
   has_many :exams, dependent: :destroy
   has_many :results, through: :exams
