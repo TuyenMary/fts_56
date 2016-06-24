9e9df542b47bc94ec46e9e817da5d2d6a9c65f36
tuyenmary <tuyenmary@gmail.com>
2016-06-24 09:02:34 +0700

    admin-add-subject

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "users/registrations"}
   root "static_pages#home"
+
+  namespace :admin do
+    root "subjects#index"
+    resources :subjects
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623041244) do
+ActiveRecord::Schema.define(version: 20160623145638) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20160623041244) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "username"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
