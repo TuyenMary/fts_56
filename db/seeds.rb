7e8922c3536df89b0c2b5a84bb6d456135c6d13a
tuyenmary <tuyenmary@gmail.com>
2016-06-29 11:21:13 +0700

    showall-edit-update-user

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,8 @@ end
 60.times do |i|
   Question.create!(content: "Question#{i+1}", subject_id: 1,
     user_id: 1)
+  2.times do |a|
+    Answer.create!(content: "Content#{a+1}", question_id: i,
+      correct: true)
   end
 end
