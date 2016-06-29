158003c1ae9ffe2e77b82da5a157a6016cc27c9f
DatDaiGia <vandat2912@gmail.com>
2016-06-29 16:50:34 +0700

    edit and delete question and answer

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,11 +1,11 @@
 class Admin::QuestionsController < ApplicationController
   load_and_authorize_resource
+  before_action :load_subject, only: [:index, :edit, :update]
 
   def index
     @questions = @questions.order(created_at: :desc)
       .page(params[:page]).per Settings.number
     @question = Question.new
-    @subjects = Subject.all
     @question.answers.build
   end
 
@@ -24,9 +24,37 @@ class Admin::QuestionsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    custom_question_params = question_params.merge!(state: "accepted")
+    if @question.update_attributes custom_question_params
+      flash[:notice] = t "admin.subject.success"
+      redirect_to admin_question_path(@question)
+    else
+      flash[:alert] = t "admin.subject.danger"
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    if @question.destroy
+      flash[:notice] = t "admin.subject.deletesuccess"
+      redirect_to admin_questions_path
+    else
+      flash[:alert] = t "admin.subject.deletefail"
+      redirect_to :back
+    end
+  end
+
   private
   def question_params
     params.required(:question).permit :content, :question_type, :subject_id,
       :user_id, answers_attributes: [:id, :correct, :content, :_destroy]
+  end
+
+  def load_subject
+    @subjects = Subject.all
   end
 end
