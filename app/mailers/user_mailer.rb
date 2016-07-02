3bca4a6c8d5d199d266a787c079fc65a387fe8a2
DatDaiGia <vandat2912@gmail.com>
2016-07-02 14:00:41 +0700

    Notify user when subject be created

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ApplicationMailer
   def inform_user_do_exam user, exam
     @user = user
-    mail to: user.email, subject: t "mailers.inform"
+    mail to: user.email, subject: t("mailers.inform")
+  end
+
+  def notify_user_after_subject_created user, subject
+    @user = user
+    @subject = subject
+    mail to: user.email, subject: "#{subject.name}"
   end
 end
